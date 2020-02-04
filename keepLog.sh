c6599230d5e74a8ea1d3326c4caf8b2cebc19f31
rhaksw <rdevco.help@gmail.com>
2020-02-04 15:15:57 +0800

    put everything into logs/

--- keepLog.sh
+++ keepLog.sh
@@ -4,10 +4,16 @@ logType=$1
 command=$2
 dateFormat="+%Y-%m-%d %H:%M:%S"
 logBase="log-$logType"
-link="$logBase.txt"
-find . -maxdepth 1 -name $link -type l -exec rm {} \;
-log=logs/log-$logType.$(date +"%Y-%m-%d_%H:%M").txt
-mkdir -p logs
-echo "$(date '+%Y-%m-%d %H:%M:%S') start [$command]" > $log
-ln -s $log $link
-nohup bash -c "$command && eval 'date \"$dateFormat\"' | tr '\n' ' ' && echo $logType finished" >> $log 2>&1 &
+logsRootDir=logs
+logDir=$logsRootDir/$logType
+mkdir -p $logDir
+linkName="$logBase.txt"
+link="$logsRootDir/$linkName"
+find $logsRootDir -name $linkName -type l -exec rm {} \;
+
+logFileName=log-$logType.$(date +"%Y-%m-%d_%H:%M:%S").txt
+logFile=$logDir/$logFileName
+logDirRelativeToLink=$logType
+echo "$(date '+%Y-%m-%d %H:%M:%S') start [$command]" > $logFile
+ln -s $logDirRelativeToLink/$logFileName $link
+nohup bash -c "$command && eval 'date \"$dateFormat\"' | tr '\n' ' ' && echo $logType finished" >> $logFile 2>&1 &
