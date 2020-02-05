c4f825c813515d5d9bacb999958a96f8e1d0cc98
rhaksw <rdevco.help@gmail.com>
2020-02-05 16:10:47 +0800

    show name of log file

--- keepLog.sh
+++ keepLog.sh
@@ -14,6 +14,7 @@ find $logsRootDir -name $linkName -type l -exec rm {} \;
 logFileName=log-$logType.$(date +"%Y-%m-%d_%H:%M:%S").txt
 logFile=$logDir/$logFileName
 logDirRelativeToLink=$logType
+echo "tail -f $link"
 echo "$(date '+%Y-%m-%d %H:%M:%S') start [$command]" > $logFile
 ln -s $logDirRelativeToLink/$logFileName $link
 nohup bash -c "$command && eval 'date \"$dateFormat\"' | tr '\n' ' ' && echo $logType finished" >> $logFile 2>&1 &
