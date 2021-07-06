c1d37328b336b990650ad7d31dbefc144dc5d7ef
rhaksw <rdevco.help@gmail.com>
2021-07-06 11:55:18 +0800

    no longer using hasura or graphql

--- loadDB.sh
+++ loadDB.sh
@@ -3,14 +3,7 @@
 mode=${1:-test} # can be [test, normal]
 dbconfig=${2:-dbconfig-example.ini} # path to config file
 
-baseCommand="python 5-load-db.py -m $mode -d $dbconfig && python 6-create-db-functions.py -m $mode -d $dbconfig"
-commandSuffix=""
-
-if [ $mode == 'normal' ] ; then
-  commandSuffix="&& ./hasuraMetadataApply.sh"
-fi
-
-command="$baseCommand $commandSuffix"
+command="python 5-load-db.py -m $mode -d $dbconfig && python 6-create-db-functions.py -m $mode -d $dbconfig"
 
 logType=db-$mode
 ./keepLog.sh $logType "$command"
