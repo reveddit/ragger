70a262122fe815fcb7bfebcb1281990b28857d73
rhaksw <rdevco.help@gmail.com>
2021-07-06 10:49:44 +0800

    more readable code

--- groupDaily.sh
+++ groupDaily.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+extension='zst'
+
 dailyDir=0-pushshift_raw_daily
 rawDir=0-pushshift_raw
+
 cd data/$rawDir
 
-ls *-*-*.gz 2>/dev/null | cut -d - -f1,2 | sort -u | while read month; do
-  files=$(ls $month-* | sort | tr '\n' ' ')
-  cat $files > $month.gz
+ls *-*-*.$extension 2>/dev/null | cut -d - -f1,2 | sort -u | while read type_year_month; do
+  files=$(ls $type_year_month-* | sort | tr '\n' ' ')
+  outputFile=$type_year_month.$extension
+  cat $files > $outputFile
   mv $files ../$dailyDir/
 done
