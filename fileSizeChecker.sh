62d384342240d7f658bdfe01b0d4130a9efd59a4
rhaksw <rdevco.help@gmail.com>
2021-07-23 08:27:44 +0800

    make fileSizeChecker work with monthly files

---
+++ fileSizeChecker.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# Usage: ./dailyFileDownloader.sh 2020-01-01 2020-06-30 data/0-pushshift_raw/
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+startDate="${1}" # date in the format 2020-01 for monthly, or 2020-01-01 for daily
+endDate="${2}" # date matching the format of startDate
+filesDir="${3:-$SCRIPT_DIR/data/0-pushshift_raw}"
+extension="${4:-.zst}"
+datePeriod=day
+validDateSuffix=""
+
+getDatePeriod() {
+  delimitersInString="${1//[0-9]}"
+  delimiterCount="${#delimitersInString}"
+  if [[ $delimiterCount -ge 1 && $delimiterCount -le 2 ]] ; then
+    if [[ $delimiterCount -eq 1 ]] ; then
+      printf month
+    else
+      printf day
+    fi
+  else
+    echo ERROR: bad format [$1]
+    exit 1
+  fi
+}
+
+period=$(getDatePeriod "$startDate")
+endPeriodType=$(getDatePeriod "$endDate")
+
+if [[ "$period" != "$endPeriodType" ]] ; then
+  echo ERROR: start and end period types do not match
+  exit 1
+fi
+
+"$SCRIPT_DIR/createDirectories.sh"
+
+fileSizesFile="$SCRIPT_DIR/remote_file_sizes.txt"
+
+if [[ ! -f "$fileSizesFile" ]] ; then
+  touch "$fileSizesFile"
+fi
+
+dateFormat="+%Y-%m-%d"
+if [[ "$period" == month ]] ; then
+  dateFormat="+%Y-%m"
+  validDateSuffix="-01"
+fi
+
+function parseDate() {
+    date="${1}${validDateSuffix}"
+    add="${2}"
+    dateToParse="${date} ${add}"
+    if [[ "$var" =~ ^[0-9]+$ ]] ; then
+      date "$dateFormat" -d@"$dateToParse";
+    else
+      date "$dateFormat" -d "$dateToParse";
+    fi
+};
+
+function sinceEpoch() {
+  dateToParse="${1}${validDateSuffix}"
+  date -d "$dateToParse" +%s
+}
+
+current=$(parseDate "$startDate")
+end=$(parseDate "$endDate")
+current_seconds=$(sinceEpoch "$current")
+end_seconds=$(sinceEpoch "$end")
+dailyFileMatch='.*-.*-'
+while (( "$current_seconds" <= "$end_seconds" ))
+do
+  file=RC_${current}${extension}
+  remoteFileSize=$(grep $file "$fileSizesFile" | awk '{print $2}')
+  if [[ -z "$remoteFileSize" ]] ; then
+    remoteFileSize=$(wget --spider --server-response -O - https://files.pushshift.io/reddit/comments/$file 2>&1 | sed -ne '/Content-Length/{s/.*: //;p}')
+    if [[ ! -z "$remoteFileSize" ]] ; then
+      if [[ "$remoteFileSize" =~ ^[0-9]+$ && $remoteFileSize -gt 1000 ]] ; then
+        echo $file $remoteFileSize >> "$fileSizesFile"
+        monthly=$(egrep -v "$dailyFileMatch" "$fileSizesFile" | grep . | sort)
+        daily=$(egrep "$dailyFileMatch" "$fileSizesFile" | sort)
+        printf "$monthly\n$daily\n" > "$fileSizesFile"
+        echo $file $remoteFileSize
+      else
+        echo ERROR: $file [$remoteFileSize] "(bad file size)"
+      fi
+    fi
+  fi
+
+  current=$(parseDate "$current" "+ 1 $period")
+  current_seconds=$(sinceEpoch "$current")
+
+  ## TODO: where should this code go?
+  ##       should be able to download all file sizes before downloading the files
+  ##
+   # localFileSize=$(stat -c %s $filesDir/$file)
+   # if [[ "$remoteFileSize" -eq "$localFileSize" ]] ; then
+   #   echo $file size match
+   # else
+   #   echo $file SIZE MISMATCH remote=$remoteFileSize
+   #   echo $file SIZE MISMATCH  local=$localFileSize
+   # fi
+
+done
